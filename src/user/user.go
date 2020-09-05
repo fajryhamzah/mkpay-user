bdbbf5685590b1fff90534f5ae61dee423d9d368
fajry hamzah <fhaji.jakmall@gmail.com>
2020-09-06 04:56:12 +0700

    docs: update user model docs

--- src/user/user.go
+++ src/user/user.go
@@ -1,5 +1,6 @@
 package user
 
+//User model
 type User struct {
 	ID       uint32
 	Email    string
@@ -9,30 +10,37 @@ type User struct {
 	password string
 }
 
+//GetID id getter
 func (u User) GetID() uint32 {
 	return u.ID
 }
 
+//GetCode code getter
 func (u User) GetCode() string {
 	return u.Code
 }
 
+//GetEmail email getter
 func (u User) GetEmail() string {
 	return u.Email
 }
 
+//GetPassword password getter
 func (u User) GetPassword() string {
 	return u.password
 }
 
+//GetActive user active getter
 func (u User) GetActive() bool {
 	return u.Active
 }
 
+//GetUserType user type getter
 func (u User) GetUserType() string {
 	return u.UserType
 }
 
+//SetPassword password setter
 func (u *User) SetPassword(password string) {
 	u.password = password
 }
