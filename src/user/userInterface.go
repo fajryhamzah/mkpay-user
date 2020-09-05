763de8012316acddb99b7d7233d31012981ef70a
fajry hamzah <fhaji.jakmall@gmail.com>
2020-09-05 22:24:46 +0700

    Update user model interface name

--- src/user/userInterface.go
+++ src/user/userInterface.go
@@ -1,9 +1,13 @@
 package user
 
+//TypeAdmin user type
 const TypeAdmin = "admin"
+
+//TypeStudent user type
 const TypeStudent = "student"
 
-type Interface interface {
+//ModelInterface user model interface
+type ModelInterface interface {
 	GetID() uint32
 	GetEmail() string
 	GetPassword() string
