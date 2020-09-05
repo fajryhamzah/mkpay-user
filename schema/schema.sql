fa7671f7aba60cefdad941e6bac4e6f43cb4588f
fajry hamzah <fhaji.jakmall@gmail.com>
2020-09-05 08:52:36 +0700

    Init project

---
+++ schema/schema.sql
@@ -0,0 +1,18 @@
+create table "user"
+(
+    id         bigserial            not null
+        constraint user_pk
+            primary key,
+    email      text                 not null,
+    password   text                 not null,
+    user_type  smallint             not null,
+    active     boolean default true not null,
+    deleted_at timestamp
+);
+
+create unique index user_email_uindex
+    on "user" (email);
+
+create unique index user_id_uindex
+    on "user" (id);
+
